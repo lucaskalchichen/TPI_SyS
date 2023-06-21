3c8fc39d635e8fd186661b2ded8f2e1a9eb16ebd
lucaskalchichen <125747543+lucaskalchichen@users.noreply.github.com>
2023-06-21 13:04:08 -0300

    Update parser.py
    primeras gramaticas

--- parser/parser.py
+++ parser/parser.py
@@ -6,65 +6,171 @@ import ply.yacc as yacc
 
 def p_document(p):
     '''document : doctype article'''
-    # Acción semántica: Verificar la gramática del documento DocBook
-    # Aquí puedes realizar las verificaciones necesarias y manejar los resultados
-
 def doctype(p):
-    'doctype : DOCTYPE_ARTICLE'
-     # Acción semántica: Verificar la gramática del documento DocBook
-    if p[1] != "<!DOCTYPE article>":
-        print("Error: El doctype no es válido.")
-    else:
-        # Realizar acciones relacionadas con el doctype
-        doctype_info = extract_doctype_info(p[1])
-        process_doctype(doctype_info)
-
-        # Realizar verificaciones y acciones adicionales para el contenido del artículo
-        article_content = p[2]
-        validate_article_content(article_content)
-
-        # Generar la salida HTML correspondiente
-        html_output = generate_html(article_content)
-        print(html_output)
-
-
+    'doctype : DOCTYPE_ARTICLE'  
 def p_article(p):
-    'article : OPEN_ARTICLE body CLOSE_ARTICLE'
-    # Acción semántica: Realizar acciones relacionadas con el artículo
+    '''
+        article : OPEN_INFO info CLOSE_INFO article1
+            |   aticle1
+    '''
 
-    p[1] ='<article>'
-    p[3] ='</article>'
-    
-    p[0]= p[1] p[2] p[3]
-    # Obtener el contenido del body
-    article_content = p[2]
-    
-    # Realizar verificaciones adicionales
-    validate_article(article_content)
-    
-    # Procesar el contenido del artículo
-    processed_content = process_article(article_content)
-    
-    # Generar la salida HTML correspondiente
-    html_output = generate_html(processed_content)
-    
-    # Imprimir la salida HTML
-    print(html_output)
+def p_article1(p):
+    '''
+        article1 :  OPEN_TITLE title ClOSE_TITLE article2
+        article1 :  article2
+    '''
+def p_article2(p):
+    '''
+        article2 : OPEN_LISTITEM itemlits CLOSE_LISTITEM article2
+            |   OPEN_IMPORTANT important CLOSE_IMPORTANT article2
+            |   OPEN_PARA  para CLOSE_PARA article2
+            |   OPEN_SIMPARA simpara CLOSE_SIMPARA article2
+            |   OPEN_ADDREESS address CLOSE_ADDRESS article2
+            |   OPEN_MEDIAOBJECT mediaobject CLOSE_MEDIAOBJECT article2
+            |   OPEN_INFORMALTABLE informaltable CLOSE_INFORMALTABLE article2
+            |   OPEN_COMMENT comment CLOSE_COMMENT article2
+            |   OPEN_ABSTRACT abstract CLOSE_ABSTRACT article2
+            |   OPEN_LISTITEM itemlits CLOSE_LISTITEM
+            |   OPEN_IMPORTANT important CLOSE_IMPORTANT
+            |   OPEN_PARA para CLOSE_PARA
+            |   OPEN_SIMPARA simpara CLOSE_SIMPARA
+            |   OPEN_ADDREESS address CLOSE_ADDRESS
+            |   OPEN_MEDIAOBJECT mediaobject CLOSE_MEDIAOBJECT
+            |   OPEN_INFORMALTABLE informaltable CLOSE_INFORMALTABLE
+            |   OPEN_COMMENT comment CLOSE_COMMENT
+            |   OPEN_ABSTRACT abstract CLOSE_ABSTRACT
+            |   OPEN_LISTITEM itemlits CLOSE_LISTITEM
+            |   OPEN_IMPORTANT important CLOSE_IMPORTANT article3
+            |   OPEN_PARA para CLOSE_PARA article3
+            |   OPEN_SIMPARA simpara CLOSE_SIMPARA article3
+            |   OPEN_ADDREESS address CLOSE_ADDRESS article3
+            |   OPEN_MEDIAOBJECT mediaobject CLOSE_MEDIAOBJECT article3
+            |   OPEN_INFORMALTABLE informaltable CLOSE_INFORMALTABLE article3
+            |   OPEN_COMMENT comment CLOSE_COMMENT article3
+            |   OPEN_ABSTRACT abstract CLOSE_ABSTRACT article3
+    '''
 
-def p_body(p):
-    '''body : section
-    
-    
-    
+def p_article3(p):
+    '''
+        article3 :  OPEN_SIMSECT simsect CLOUSE_SIMPLESECT 
+        |   OPEN_SECTION section CLOUSE_SECTION
+
+        | OPEN_SIMSECT CLOUSE_SIMPLESECT article3simsect
+        | OPEN_SECTION section CLOUSE_SECTION article3section
     
     '''
-    # Acción semántica: Realizar acciones relacionadas con el cuerpo del documento
+def p_article3section(p):
+    '''
+    article3section:    OPEN_SECTION section CLOUSE_SECTION article3section
+    | OPEN_SECTION  section CLOUSE_SECTION
+    '''
+def p_article3(p):
+    '''
+    article3simsect : OPEN_SIMSECT simsect CLOUSE_SIMPLESECT article3simsect
+        |   OPEN_SIMSECT simsect CLOUSE_SIMPLESECT 
+    '''
 
 def p_section(p):
-    '''section : OPEN_SECTION title body CLOSE_SECTION'''
-    # Acción semántica: Realizar acciones relacionadas con la sección
+    '''
+        section : OPEN_INFO info CLOSE_INFO section1
+            |   section1
+    '''
 
-def p_sim_section(p);
+
+def p_section1(p):
+    '''
+        section1 :  OPEN_TITLE title ClOSE_TITLE section2
+            |   section 2
+    '''
+
+def p_section2(p):
+    '''
+        section2 : OPEN_LISTITEM itemlits CLOSE_LISTITEM section2
+            |   OPEN_IMPORTANT important CLOSE_IMPORTANT section2
+            |   OPEN_PARA  para CLOSE_PARA section2
+            |   OPEN_SIMPARA simpara CLOSE_SIMPARA section2
+            |   OPEN_ADDREESS address CLOSE_ADDRESS section2
+            |   OPEN_MEDIAOBJECT mediaobject CLOSE_MEDIAOBJECT section2
+            |   OPEN_INFORMALTABLE informaltable CLOSE_INFORMALTABLE section2
+            |   OPEN_COMMENT comment CLOSE_COMMENT section2
+            |   OPEN_ABSTRACT abstract CLOSE_ABSTRACT section2
+            |   OPEN_LISTITEM itemlits CLOSE_LISTITEM
+            |   OPEN_IMPORTANT important CLOSE_IMPORTANT
+            |   OPEN_PARA para CLOSE_PARA
+            |   OPEN_SIMPARA simpara CLOSE_SIMPARA
+            |   OPEN_ADDREESS address CLOSE_ADDRESS
+            |   OPEN_MEDIAOBJECT mediaobject CLOSE_MEDIAOBJECT
+            |   OPEN_INFORMALTABLE informaltable CLOSE_INFORMALTABLE
+            |   OPEN_COMMENT comment CLOSE_COMMENT
+            |   OPEN_ABSTRACT abstract CLOSE_ABSTRACT
+            |   OPEN_LISTITEM itemlits CLOSE_LISTITEM
+            |   OPEN_IMPORTANT important CLOSE_IMPORTANT section3
+            |   OPEN_PARA para CLOSE_PARA section3
+            |   OPEN_SIMPARA simpara CLOSE_SIMPARA section3
+            |   OPEN_ADDREESS address CLOSE_ADDRESS section3
+            |   OPEN_MEDIAOBJECT mediaobject CLOSE_MEDIAOBJECT section3
+            |   OPEN_INFORMALTABLE informaltable CLOSE_INFORMALTABLE section3
+            |   OPEN_COMMENT comment CLOSE_COMMENT section3
+            |   OPEN_ABSTRACT abstract CLOSE_ABSTRACT section3
+    '''
+
+def p_section3(p):
+    '''
+    section3 :  OPEN_SIMSECT simsect CLOUSE_SIMPLESECT
+        |   OPEN_SECTION section CLOUSE_SECTION
+
+        | OPEN_SIMSECT CLOUSE_SIMPLESECT section3simsect
+        | OPEN_SECTION section CLOUSE_SECTION section3section
+    '''
+
+def p_section3section(p):
+    '''
+    section3section:    OPEN_SECTION section CLOUSE_SECTION section3section
+    | OPEN_SECTION  section CLOUSE_SECTION
+    '''
+
+def p_section3simsect(p):
+    '''
+    section3simsect : OPEN_SIMSECT simsect CLOUSE_SIMPLESECT section3simsect
+        |   OPEN_SIMSECT simsect CLOUSE_SIMPLESECT
+    '''
+
+def p_simplesec(p):
+    '''
+        simplesec : OPEN_INFO info CLOSE_INFO simplesec1
+            |   simplesec1
+    '''
+
+
+def p_simplesec1(p):
+    '''
+        simplesec1 :  OPEN_TITLE title ClOSE_TITLE simplesec2
+            |   simplesec2
+    '''
+
+def p_simplesec2(p):
+    '''
+        simplesec2 : OPEN_LISTITEM itemlits CLOSE_LISTITEM simplesec2
+            |   OPEN_IMPORTANT important CLOSE_IMPORTANT simplesec2
+            |   OPEN_PARA  para CLOSE_PARA simplesec2
+            |   OPEN_SIMPARA simpara CLOSE_SIMPARA simplesec2
+            |   OPEN_ADDREESS address CLOSE_ADDRESS simplesec2
+            |   OPEN_MEDIAOBJECT mediaobject CLOSE_MEDIAOBJECT simplesec2
+            |   OPEN_INFORMALTABLE informaltable CLOSE_INFORMALTABLE simplesec2
+            |   OPEN_COMMENT comment CLOSE_COMMENT simplesec2
+            |   OPEN_ABSTRACT abstract CLOSE_ABSTRACT simplesec2
+            |   OPEN_LISTITEM itemlits CLOSE_LISTITEM
+            |   OPEN_IMPORTANT important CLOSE_IMPORTANT
+            |   OPEN_PARA para CLOSE_PARA
+            |   OPEN_SIMPARA simpara CLOSE_SIMPARA
+            |   OPEN_ADDREESS address CLOSE_ADDRESS
+            |   OPEN_MEDIAOBJECT mediaobject CLOSE_MEDIAOBJECT
+            |   OPEN_INFORMALTABLE informaltable CLOSE_INFORMALTABLE
+            |   OPEN_COMMENT comment CLOSE_COMMENT
+            |   OPEN_ABSTRACT abstract CLOSE_ABSTRACT
+            |   OPEN_LISTITEM itemlits CLOSE_LISTITEM
+    '''
+
 
 def p_title(p):
     '''title : OPEN_TITLE TEXT CLOSE_TITLE'''
@@ -72,9 +178,6 @@ def p_title(p):
 
 # Otras reglas gramaticales y acciones semánticas...
 
-def p_article(p):
-    'article : OPEN_INFO info CLOSE_INFO article'
-    p[0] = <info> p[1] </info> p[3]
 
 
 # Manejo de errores
