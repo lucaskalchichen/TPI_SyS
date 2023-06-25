ca248162eccbf38a8d51c5c7f7696031205d6f0e
ValenHonn <133966452+ValenHonn@users.noreply.github.com>
2023-06-25 14:02:28 -0300

    Update parser.py

--- parser/parser.py
+++ parser/parser.py
@@ -231,16 +231,206 @@ def p_copyright3(p):
         copyright3 : OPEN_HOLDER holder CLOSE_HOLDER copyright3
             |   OPEN_HOLDER holder CLOSE_HOLDER
     '''
-
-
-
 def p_title(p):
-    '''title : OPEN_TITLE TEXT CLOSE_TITLE'''
-    # Acción semántica: Realizar acciones relacionadas con el título
+   '''
+        title : OPEN_TEXT text CLOSE_TEXT title
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS title
+            |   #falta link aca
+            |   OPEN_EMAIL email CLOSE_EMAIL title
+            |   OPEN_TEXT text CLOSE_TEXT
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS
+            |   #falta link aca
+            |   OPEN_EMAIL email CLOSE_EMAIL
 
-# Otras reglas gramaticales y acciones semánticas...
+    '''
+def p_simpara(p):
+    '''
+        simpara : OPEN_TEXT text CLOSE_TEXT simpara
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS simpara
+            |   #falta link aca
+            |   OPEN_EMAIL email CLOSE_EMAIL simpara
+            |   OPEN_AUTHOR author CLOSE_AUTHOR simpara
+            |   OPEN_COMMENT comment CLOSE_COMMENT simpara
+            |   OPEN_TEXT text CLOSE_TEXT
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS
+            |   #falta link aca
+            |   OPEN_EMAIL email CLOSE_EMAIL 
+            |   OPEN_AUTHOR author CLOSE_AUTHOR 
+            |   OPEN_COMMENT comment CLOSE_COMMENT
 
+    '''
+def p_emphasis(p):
+    '''
+        emphasis : OPEN_TEXT text CLOSE_TEXT emphasis
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS emphasis
+            |   #falta link aca
+            |   OPEN_EMAIL email CLOSE_EMAIL emphasis
+            |   OPEN_AUTHOR author CLOSE_AUTHOR emphasis
+            |   OPEN_COMMENT comment CLOSE_COMMENT emphasis
+            |   OPEN_TEXT text CLOSE_TEXT
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS
+            |   #falta link aca
+            |   OPEN_EMAIL email CLOSE_EMAIL 
+            |   OPEN_AUTHOR author CLOSE_AUTHOR 
+            |   OPEN_COMMENT comment CLOSE_COMMENT
 
+    '''
+def p_comment(p):
+    '''
+        comment : OPEN_TEXT text CLOSE_TEXT comment
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS comment
+            |   #falta link aca
+            |   OPEN_EMAIL email CLOSE_EMAIL comment
+            |   OPEN_AUTHOR author CLOSE_AUTHOR comment
+            |   OPEN_COMMENT comment CLOSE_COMMENT comment
+            |   OPEN_TEXT text CLOSE_TEXT
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS
+            |   #falta link aca
+            |   OPEN_EMAIL email CLOSE_EMAIL 
+            |   OPEN_AUTHOR author CLOSE_AUTHOR 
+            |   OPEN_COMMENT comment CLOSE_COMMENT
+
+    '''
+
+def p_important(p):
+     '''
+        important : OPEN_TITLE title CLOSE_TITLE important2
+            |   important2
+
+    '''
+
+def p_important2(p):
+     '''
+        important2 : OPEN_LISTITEM itemlist CLOSE_LISTITEM important2
+            |   OPEN_IMPORTANT important CLOSE_IMPORTANT important2
+            |   OPEN_PARA para CLOSE_PARA important2
+            |   OPEN_SIMPARA simpara CLOSE_SIMPARA important2
+            |   OPEN_ADDRESS address CLOSE_ADDRESS important2
+            |   OPEN_MEDIAOBJECT mediaobject CLOSE_MEDIAOBJECT important2
+            |   OPEN_INFORMALTABLE informaltable CLOSE_INFORMALTABLE important2
+            |   OPEN_COMMENT comment CLOSE_COMMENT important2
+            |   OPEN_ABSTRACT abstract CLOSE_ABSTRACT important2
+            |   OPEN_LISTITEM itemlist CLOSE_LISTITEM
+            |   OPEN_IMPORTANT important CLOSE_IMPORTANT
+            |   OPEN_PARA para CLOSE_PARA
+            |   OPEN_SIMPARA simpara CLOSE_SIMPARA
+            |   OPEN_ADDRESS address CLOSE_ADDRESS
+            |   OPEN_MEDIAOBJECT mediaobject CLOSE_MEDIAOBJECT
+            |   OPEN_INFORMALTABLE informaltable CLOSE_INFORMALTABLE
+            |   OPEN_COMMENT comment CLOSE_COMMENT
+            |   OPEN_ABSTRACT abstract CLOSE_ABSTRACT
+
+    '''
+def p_firstname(p):
+    '''
+        firstname : OPEN_TEXT text CLOSE_TEXT firstname
+            |   #falta link aca
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS firstname
+            |   OPEN_COMMENT comment CLOSE_COMMENT firstname
+            |   OPEN_TEXT text CLOSE_TEXT
+            |   #falta link aca
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS
+            |   OPEN_COMMENT comment CLOSE_COMMENT
+    '''
+def p_surname(p):
+    '''
+        surname : OPEN_TEXT text CLOSE_TEXT surname
+            |   #falta link aca
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS surname
+            |   OPEN_COMMENT comment CLOSE_COMMENT surname
+            |   OPEN_TEXT text CLOSE_TEXT
+            |   #falta link aca
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS
+            |   OPEN_COMMENT comment CLOSE_COMMENT
+    '''
+def p_street(p):
+     '''
+        street : OPEN_TEXT text CLOSE_TEXT street
+            |   #falta link aca
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS street
+            |   OPEN_COMMENT comment CLOSE_COMMENT street
+            |   OPEN_TEXT text CLOSE_TEXT
+            |   #falta link aca
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS
+            |   OPEN_COMMENT comment CLOSE_COMMENT
+    '''
+def p_city(p):
+    '''
+        city : OPEN_TEXT text CLOSE_TEXT city
+            |   #falta link aca
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS city
+            |   OPEN_COMMENT comment CLOSE_COMMENT city
+            |   OPEN_TEXT text CLOSE_TEXT
+            |   #falta link aca
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS
+            |   OPEN_COMMENT comment CLOSE_COMMENT
+    '''
+def p_state(p):
+    '''
+        state : OPEN_TEXT text CLOSE_TEXT state
+            |   #falta link aca
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS state
+            |   OPEN_COMMENT comment CLOSE_COMMENT state
+            |   OPEN_TEXT text CLOSE_TEXT
+            |   #falta link aca
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS
+            |   OPEN_COMMENT comment CLOSE_COMMENT
+    '''
+def p_phone(p):
+     '''
+        phone : OPEN_TEXT text CLOSE_TEXT phone
+            |   #falta link aca
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS phone
+            |   OPEN_COMMENT comment CLOSE_COMMENT phone
+            |   OPEN_TEXT text CLOSE_TEXT
+            |   #falta link aca
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS
+            |   OPEN_COMMENT comment CLOSE_COMMENT
+    '''
+def p_email(p):
+    '''
+        email : OPEN_TEXT text CLOSE_TEXT email
+            |   #falta link aca
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS email
+            |   OPEN_COMMENT comment CLOSE_COMMENT email
+            |   OPEN_TEXT text CLOSE_TEXT
+            |   #falta link aca
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS
+            |   OPEN_COMMENT comment CLOSE_COMMENT
+    '''
+def p_date(p):
+    '''
+        date : OPEN_TEXT text CLOSE_TEXT date
+            |   #falta link aca
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS date
+            |   OPEN_COMMENT comment CLOSE_COMMENT date
+            |   OPEN_TEXT text CLOSE_TEXT
+            |   #falta link aca
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS
+            |   OPEN_COMMENT comment CLOSE_COMMENT
+    '''
+def p_year(p):
+     '''
+        year : OPEN_TEXT text CLOSE_TEXT year
+            |   #falta link aca
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS year
+            |   OPEN_COMMENT comment CLOSE_COMMENT year
+            |   OPEN_TEXT text CLOSE_TEXT
+            |   #falta link aca
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS
+            |   OPEN_COMMENT comment CLOSE_COMMENT
+    '''
+def p_holder(p):
+    '''
+        holder : OPEN_TEXT text CLOSE_TEXT holder
+            |   #falta link aca
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS holder
+            |   OPEN_COMMENT comment CLOSE_COMMENT holder
+            |   OPEN_TEXT text CLOSE_TEXT
+            |   #falta link aca
+            |   OPEN_EMPHASIS emphasis CLOSE_EMPHASIS
+            |   OPEN_COMMENT comment CLOSE_COMMENT
+    '''
 
 # Manejo de errores
 def p_error(p):
